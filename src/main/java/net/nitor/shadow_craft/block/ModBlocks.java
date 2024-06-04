356c39826b8d8f37bfcb17a17ec6f4218b1060e8
Nitor112221 <kostya1234moi@gmail.com>
2024-06-04 14:35:07 +0500

    добавлены таблицы лута

--- src/main/java/net/nitor/shadow_craft/block/ModBlocks.java
+++ src/main/java/net/nitor/shadow_craft/block/ModBlocks.java
@@ -1,9 +1,11 @@
 package net.nitor.shadow_craft.block;
 
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -22,7 +24,7 @@ public class ModBlocks {
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).sound(SoundType.DEEPSLATE)));
 
     public static final RegistryObject<Block> SHADOW_ROCK_ORE = registerBlock("shadow_rock_ore",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_IRON_ORE).sound(SoundType.DEEPSLATE)));
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_IRON_ORE).sound(SoundType.DEEPSLATE), UniformInt.of(3, 6)));
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
